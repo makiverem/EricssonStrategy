a4f8f8f7ab0ca9a47c7aa9051331d50bd68a8773
galovics <galovicsarnold@gmail.com>
2013-11-20 02:28:10 +0100

    asd

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2013.11.13.
+ *      Author: Galovics
+ */
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello world!d" << std::endl;
+	return 0;
+}
+
+
